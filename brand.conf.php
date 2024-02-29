dee2d2867484607d409ec5c9b1314b752b417c60
Jonathan Lima <lima_jonathan@hotmail.com>
2024-02-29 16:34:33 -0300

    commit

--- brand.conf.php
+++ brand.conf.php
@@ -4,7 +4,7 @@ return [
         'BRAND_LOGO' => './assets/img/sermais.png',
         'BRAND_LOGO_SIDEBAR' => './assets/img/sermais.png',
         'BRAND_LOGO_SIDEBAR_COMPACT' => './assets/img/sermais.png',
-        'BRAND_FOOTER' => '@Sermais Tecnologia',
+        'BRAND_FOOTER' => '@Sermais Tecnologia - 2024',
         'BRAND_HELP_URL' => 'https://sermaistecnologia.com.br',
 ];
 
